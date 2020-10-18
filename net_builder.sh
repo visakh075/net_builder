f5f5b4f4df28621a1cb21e67cb755db7dcb5a6c6
hermit <hermit@hermitsdeb>
2020-10-18 17:31:39 +0530

    working

--- net_builder.sh
+++ net_builder.sh
@@ -3,3 +3,32 @@
 #
 # runs on directory runs make_all.sh file
 # copy the result files to builder_box folder
+net_build(){
+	echo "FILELIST"
+	AR=`arch` 
+	echo $AR
+	#mkdir "net_build${ar}" 
+	while read p
+	do
+		echo $p
+	done < list.net_build
+}
+checklist(){
+	if test -f list.net_build
+	then
+		echo "Valid list"
+		net_build
+	else
+		echo "No list found"
+		exit
+	fi
+}
+if test -f make_all.sh
+then
+	echo "Valid path"
+	checklist
+else
+	echo "Invalid path"
+	exit
+fi
+exit
